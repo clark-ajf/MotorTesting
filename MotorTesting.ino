2955ef4ea19ca39008b7b7873c73f952a52a148a
Clark Jeria <clark.ajf@gmail.com>
2020-03-21 20:13:12 -0700

    Adding Radio Receiver for Joystick

--- MotorTesting.ino
+++ MotorTesting.ino
@@ -1,54 +1,62 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 
-//Joystick Pins
-int x_key = A0;                                               
-int y_key = A1;                                               
-int x_pos;
-int y_pos;
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+//LED Pin
+#define LED 13;
+
+//Motor Pins
+#define EN_A 11;      //Enable pin for first motor
+#define IN1 9;       //control pin for first motor
+#define IN2 8;       //control pin for first motor
+#define IN3 7;        //control pin for second motor
+#define IN4 6;        //control pin for second motor
+#define EN_B 10;      //Enable pin for second motor
+
+uint8_t motorcontrol[3];
+
+#define DIRECTION 2;
+#define MOTOR_RIGHT 1;
+#define MOTOR_LEFT 0;
+
+//nRF24L01 Pins
+#define CE_PIN = 7;
+#define SCK_PIN = 13;
+#define MISO_PIN = 12;
+#define CSN_PIN  = 8;
+#define MOSI_PIN = 11; 
+
+RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
+const byte address[6] = "00001";
 
 bool joystick_enabled = true;
 
-//LED Pin
-int LED = 13;
-
-//Motor Pins
-int EN_A = 11;      //Enable pin for first motor
-int IN1 = 9;       //control pin for first motor
-int IN2 = 8;       //control pin for first motor
-int IN3 = 7;        //control pin for second motor
-int IN4 = 6;        //control pin for second motor
-int EN_B = 10;      //Enable pin for second motor
-
-//Initializing variables to store data
-int motor_horizontal;
-int motor_vertical;
 
 ros::NodeHandle nh;
 std_msgs::String str_msg;
 void joystickCallback(const std_msgs::String& joystick_command_msg) {
     nh.loginfo(joystick_command_msg.data);
-    if(strcmp(joystick_command_msg.data,"LEFT") == 0 && !joystick_enabled) {
-        x_pos = 200;
-        left();
-    } else if (strcmp(joystick_command_msg.data,"RIGHT") == 0 && !joystick_enabled) {
-        x_pos = 800;
-        right(); 
-    } else if (strcmp(joystick_command_msg.data,"DOWN") == 0 && !joystick_enabled) {
-        y_pos = 200;
-	    down();
-    } else if (strcmp(joystick_command_msg.data,"UP") == 0 && !joystick_enabled) {
-        y_pos = 800;
-        up();
+
+    if(strcmp(joystick_command_msg.data,"LEFT") == 0 && !joystick_enabled) {     
+        drive(0, 150, 50);
+    } else if (strcmp(joystick_command_msg.data,"RIGHT") == 0 && !joystick_enabled) {        
+        drive(0, 50, 150);
+    } else if (strcmp(joystick_command_msg.data,"BACKWARD") == 0 && !joystick_enabled) {
+        drive(1, 100, 100);
+    } else if (strcmp(joystick_command_msg.data,"FORWARD") == 0 && !joystick_enabled) {
+        drive(0, 100, 100);
     } else if (strcmp(joystick_command_msg.data,"STOP") == 0 && !joystick_enabled) {
-        x_pos = 500;
-        y_pos = 500;
         stay(); 
     } else if (strcmp(joystick_command_msg.data,"DISABLE_JOYSTICK") == 0) {
         joystick_enabled = false;
     } else if (strcmp(joystick_command_msg.data,"ENABLE_JOYSTICK") == 0) {
         joystick_enabled = true;
     }
+
     nh.loginfo(joystick_enabled ? "TRUE" : "FALSE");
     publish_joystick_sensor(joystick_command_msg);
 }
@@ -56,7 +64,15 @@ ros::Publisher pub_joystick("joystick_sensor", &str_msg);
 ros::Subscriber<std_msgs::String> sub_joystick("joystick_commands", joystickCallback);
 
 void setup ( ) {
-  Serial.begin (9600); //Starting the serial communication at 9600 baud rate
+  //Starting the serial communication at 9600 baud rate
+  Serial.begin (9600);
+
+  // Initializing Radio Receiver
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
   //Initializing the motor pins as output
   pinMode(EN_A, OUTPUT);
   pinMode(IN1, OUTPUT);  
@@ -64,10 +80,6 @@ void setup ( ) {
   pinMode(IN3, OUTPUT);  
   pinMode(IN4, OUTPUT);
   pinMode(EN_B, OUTPUT);
-
-  //Initializing the joystick pins as input
-  pinMode(x_key, INPUT) ;                     
-  pinMode(y_key, INPUT) ;    
 
   //Initializing LED pin
   pinMode(LED, OUTPUT);
@@ -81,34 +93,15 @@ void setup ( ) {
 
 void loop () {
     if(joystick_enabled){
-        x_pos = analogRead (x_key) ;  //Reading the horizontal movement value
-        y_pos = analogRead (y_key) ;  //Reading the vertical movement value
-    
-        if (x_pos < 400){     //Rotating the left motor in clockwise direction
-            left();
-            str_msg.data = "LEFT";
-            publish_joystick_sensor(str_msg);
-        }
-        else if (x_pos>400 && x_pos <600){  //Motors will not move when the joystick will be at center
-            stay();
-        }  
-        else if (x_pos > 600){    //Rotating the left motor in anticlockwise direction
-            right();
-            str_msg.data = "RIGHT";
-            publish_joystick_sensor(str_msg);
-        }   
-        if (y_pos < 400){         //Rotating the right motor in clockwise direction
-            down();
-            str_msg.data = "DOWN";
-            publish_joystick_sensor(str_msg);
-        }
-        else if (y_pos>400 && y_pos <600){
-            stay();   
-        }  
-        else if (y_pos > 600){        //Rotating the right motor in anticlockwise direction
-            up();
-            str_msg.data = "UP";
-            publish_joystick_sensor(str_msg);
+        if (radio.available()) {
+            radio.read(&motorcontrol, sizeof(motorcontrol));
+            drive(motorcontrol[DIRECTION], motorcontrol[MOTOR_RIGHT], motorcontrol[MOTOR_LEFT]);
+            Serial.print("Motor LEFT: ");
+            Serial.print(motorcontrol[MOTOR_LEFT]);
+            Serial.print(" - Motor RIGHT: ");
+            Serial.print(motorcontrol[MOTOR_RIGHT]);
+            Serial.print(" - Direction: ");
+            Serial.println(motorcontrol[DIRECTION]);
         }
     }
     
@@ -124,42 +117,30 @@ void publish_joystick_sensor(const std_msgs::String& joystick_sensor_msg) {
     }
 }
 
-void left() {
-    motor_horizontal = map(x_pos, 400, 0, 0, 255);   //Mapping the values to 0-255 to move the motor
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    analogWrite(EN_A, motor_horizontal);
-    digitalWrite(LED, HIGH);
+void drive(int direction, int motor_right_speed, int motor_left_speed) {
+    if (direction == 1){ // Backward
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+    } else { // Forward
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+    }
+
+    // Drive Motors        
+    analogWrite(EN_A, motor_right_speed);
+    analogWrite(EN_B, motor_left_speed);
 }
 
-void right() {
-    motor_horizontal = map(x_pos, 600, 1023, 0, 255);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(EN_A, motor_horizontal);
-    digitalWrite(LED, HIGH);
-}
-
-void up() {
-    motor_vertical = map(y_pos, 600, 1023, 0, 255);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    analogWrite(EN_B, motor_vertical);
-    digitalWrite(LED, HIGH);
-}
-
-void down() {
-    motor_vertical = map(y_pos, 400, 0, 0, 255);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-    analogWrite(EN_B, motor_vertical);
-    digitalWrite(LED, HIGH);
-}
-
-void stay() {
+void stop() {
     analogWrite(EN_A, 0);
     analogWrite(EN_B, 0);
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
     digitalWrite(LED, LOW);
 }
